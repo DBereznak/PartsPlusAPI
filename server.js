baadacc7e20ca9ec63c262b616cfef897c37ecaf
dbereznak <dbereznak@gmail.com>
2021-03-29 22:02:25 -0400

    added pacth to CORS

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS, HEAD"
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD"
   );
   res.header(
     "Access-Control-Allow-Headers",
